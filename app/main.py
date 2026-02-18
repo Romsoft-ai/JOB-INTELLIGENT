ef832e15fe51f2347383118d50728e158d67b3c6
Romuald-Albert <romuald.noubissi.pro@gmail.com>
2026-02-18 11:54:48 +0100

    version plus propre de l'interface streamlit.

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, UploadFile, File
+import os
 
 app = FastAPI(
     title="Job Intelligent App",
@@ -6,12 +7,20 @@ app = FastAPI(
     version="0.1.0"
 )
 
+UPLOAD_DIR = "./app/tmp_uploads"
+os.makedirs(UPLOAD_DIR, exist_ok=True)
 
 @app.get("/")
 def root():
     return {"message": "Bienvenue sur Job Intelligent App API"}
 
-
 @app.get("/health")
 def health_check():
     return {"status": "ok"}
+
+@app.post("/upload-cv")
+def upload_cv(file: UploadFile = File(...)):
+    file_location = os.path.join(UPLOAD_DIR, file.filename)
+    with open(file_location, "wb") as f:
+        f.write(file.file.read())
+    return {"filename": file.filename, "saved_path": file_location, "status": "uploaded"}
